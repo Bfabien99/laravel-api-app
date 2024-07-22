e62742674c4cdbfc9f7d9acfc9fb0c0c7db4462c
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-07-22 17:09:39 +0000

    enregistrement de l'utilisateur

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
     public function register(Request $request){
-        return 'register';
+        $fields = $request->validate([
+            'name' => 'required|max:200',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+        $user = User::create($fields);
+        return ['message'=>'register ok', 'datas' => $user];
     }
 
     public function login(Request $request){
