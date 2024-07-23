1bc6655ad3b52caa67b5e6df8a97e952c32edd07
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-07-23 08:38:46 +0000

    protection des routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,7 +36,7 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request){
-        $request->user()->tokens()->delete;
+        $request->user()->tokens()->delete();
         return [
             'message' => 'You are logged out',
         ];
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ Route::controller(AuthController::class)->group(function(){
     Route::prefix('auth')->group(function(){
         Route::post('/register', 'register');
         Route::post('/login', 'login');
-        Route::post('/logout', 'logout');
+        Route::post('/logout', 'logout')->middleware(['auth:sanctum']);
     });
 });
